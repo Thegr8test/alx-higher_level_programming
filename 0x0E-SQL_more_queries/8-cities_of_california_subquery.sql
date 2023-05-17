f4553fb22bc319bb8ccc4a5c0735c5589e3be071
thegr8test <wilsonjulius36@gamil.com>
2023-05-17 13:07:17 -0700

    more queries

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- Lists all particulars of a database
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
+ 
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Lists all contents of a data base
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
+ 
